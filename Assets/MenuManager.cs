3f03a5ff69d5c716c14ab0893522e45b08001652
Ryguyrocks22 <70034410+Ryguyrocks22@users.noreply.github.com>
2021-03-30 23:32:54 -0400

    Hopefully this works

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public string levelToLoad = "GameScene";
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
